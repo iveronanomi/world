ecfe49644fa4f66a7b0db24e50650749c71709fe
Eremin Ivan <coding.ebola@gmail.com>
2016-01-20 12:08:08 +0300

    Some changes

--- src/player/emotions_test.go
+++ src/player/emotions_test.go
@@ -2,35 +2,33 @@ package player
 
 import (
 	"testing"
-	"fmt"
 )
 
-func TestProcess (t *testing.T) {
+func TestProcess(t *testing.T) {
 	cases := []struct {
-		applet			Applet
-		expectResult	Emotion
+		applet       Applet
+		expectResult Emotion
 	}{
 		{
-			applet : Applet{
-				Agent:Person{Name: "Alice"},
-				Recipient:Person{Name: "Alice"},
-				Action: Verb{Name: "Kill", SocialStigma: -0.8, Attitude: -0.5},
-				Possibility:0.9,
+			applet: Applet{
+				Agent:       Person{Name: "Alice"},
+				Recipient:   Person{Name: "Alice"},
+				Action:      Verb{Name: "Kill", SocialStigma: -0.8, Attitude: -0.5},
+				Possibility: 0.9,
 			},
 			expectResult: EMOTION_SHAME | EMOTION_DISTRESS,
 		},
 		{
-			applet : Applet{
-				Agent:Person{Name: "Alice"},
-				Recipient:Person{Name: "Bob"},
-				Action: Verb{Name: "Search", SocialStigma: -0.8, Attitude: -0.5},
-				Possibility:0.9,
+			applet: Applet{
+				Agent:       Person{Name: "Alice"},
+				Recipient:   Person{Name: "Bob"},
+				Action:      Verb{Name: "Search", SocialStigma: -0.8, Attitude: -0.5},
+				Possibility: 0.9,
 			},
 			expectResult: EMOTION_ANGER | EMOTION_DISTRESS,
 		},
 	}
 	for _, c := range cases {
-		fmt.Println(c.applet.Process())
 		if c.applet.Process() != c.expectResult {
 			t.Fail()
 		}
--- src/player/person.go
+++ src/player/person.go
@@ -1,18 +1,18 @@
 package player
 
 type Person struct {
-	Name					string
-	Age						int8
-	PsychoemotionalFeatures	PsychoemotionalFeatures
-	Emotions				Emotions
+	Name string
+	Age  int8
+	PsychoEmotionalFeatures
+	Emotions
 }
 
 type Emotions map[Emotion]float32
 
-type PsychoemotionalFeatures struct {
-	Openness			float32
-	Conscientiousness	float32
-	Extraversion		float32
-	Pliability			float32
-	Neuroticism			float32
+type PsychoEmotionalFeatures struct {
+	Openness          float32
+	Conscientiousness float32
+	Extroversion      float32
+	Pliability        float32
+	Neuroticism       float32
 }
